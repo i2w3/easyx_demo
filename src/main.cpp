07354be9b17c26f3a551937acbe2820fa337d0f5
dell <MedRobGDUT@outlook.com>
2024-03-03 16:50:14 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main()
 {
     printf("Hello, world\n");
     initgraph(640, 480);	// 创建绘图窗口，大小为 640x480 像素
-	circle(200, 200, 100);	// 画圆，圆心(200, 200)，半径 100
-	_getch();				// 按任意键继续
-	closegraph();			// 关闭绘图窗口
+    circle(200, 200, 100);	// 画圆，圆心(200, 200)，半径 100
+    _getch();				// 按任意键继续
+    closegraph();			// 关闭绘图窗口
     return 0;
 }
